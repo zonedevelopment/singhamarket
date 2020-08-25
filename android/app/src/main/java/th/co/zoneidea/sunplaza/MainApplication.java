1a61a604fd4820ecd434e64cb0bc2de0a03bdd92
ธีรยุทธ  กลิ่นสง่า <teerayut.k@thiensurat.co.th>
2020-08-25 21:08:32 +0700

    Commit 25/08/2020 Edit MainApplication

--- android/app/src/main/java/th/co/zoneidea/sunplaza/MainApplication.java
+++ android/app/src/main/java/th/co/zoneidea/sunplaza/MainApplication.java
@@ -3,7 +3,7 @@ package th.co.zoneidea.sunplaza;
 import android.app.Application;
 import android.content.Context;
 
-import com.facebook.react.BuildConfig;
+//import com.facebook.react.BuildConfig;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.imagepicker.ImagePickerPackage;
